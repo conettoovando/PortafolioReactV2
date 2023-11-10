96e632ed02092020548819e57e401079c469a005
conettoovando <cris.onetto01@gmail.com>
2023-11-10 16:03:42 -0300

    Update CSS styles for various components and update for json

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-scroll';
 import audioOrg from '../assets/sonido2.mp3'
 
 export default function Navbar() {
-    const playSound = () => {
+    const playSound = (route) => {
+        const selected = document.getElementsByClassName('active').item(0).innerHTML;
+        if (selected === route) return;
         const audio = new Audio(audioOrg);
         audio.volume = 0.05;
         audio.currentTime = 0.5;
         audio.play();
+        document.getElementById('menu-toggle').checked = false;
     };
 
     return (
@@ -16,61 +19,56 @@ export default function Navbar() {
             <label className='menu-button-container' htmlFor='menu-toggle'>
                 <div className='burger'></div>
             </label>
-                <ul className='ulNav'>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="Home"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className='link'
-                            onClick={() => playSound()}
-                        >
-                            Inicio
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="SobreMi"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className='link'
-                            onClick={() => playSound()}
-                        >
-                            Sobre mi
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="Proyectos"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className='link'
-                            onClick={() => playSound()}
-                        >
-                            Proyectos
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="Contacto"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className='link'
-                            onClick={() => playSound()}
-                        >
-                            Contacto
-                        </Link>
-                    </li>
-                </ul>
-
+            <ul className='ulNav'>
+              <li>
+                <Link
+                  to="Home"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className='link'
+                  onClick={() => playSound('Inicio')}
+                >
+                  Inicio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="SobreMi"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className='link'
+                  onClick={() => playSound('Sobre mi')}
+                >
+                  Sobre mi
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="Proyectos"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className='link'
+                  onClick={() => playSound('Proyectos')}
+                >
+                  Proyectos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="Contacto"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className='link'
+                  onClick={() => playSound('Contacto')}
+                >
+                  Contacto
+                </Link>
+              </li>
+            </ul>
         </nav>
     )
 }
